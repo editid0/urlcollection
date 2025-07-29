d3e6977fa362c1e144fa3e96f7d2ac2d8996d04a
editid0 <slyfoxdiscordbot@gmail.com>
2025-07-29 02:13:19 +0100

    homepage done, yay

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # urlcollection
 A place to store your URLs.
+
+# AI Usage
+AI tab complete is used to fill out repetitive functions, other than that, all code is written by hand.
---
+++ urlcollect/app/page.jsx
@@ -0,0 +1,11 @@
+export default function Home() {
+	return (
+		<>
+			<div>
+				<h1 className="text-3xl font-bold underline">
+					URL Collector
+				</h1>
+			</div>
+		</>
+	)
+}
